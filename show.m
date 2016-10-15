d905947423b5da713514660acff909b3600ab349
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-15 16:34:44 +0200

    Refine the scripts

--- show.m
+++ show.m
@@ -2,12 +2,12 @@ function show(name)
   setup;
   [data, ~] = aggregate(locate(name), 'dynamic_power');
   [units, steps] = size(data);
+  time = 1e-3 * (0:(steps - 1));
   Plot.figure(1000, units * 200);
   for i = 1:units
     subplot(units, 1, i);
-    plot(data(i, :), 'LineWidth', 1);
-    Plot.limit([0, steps]);
-    set(gca,'xtick', [])
-    set(gca,'xticklabel', [])
+    Plot.line(time, data(i, :), 'style', {'LineWidth', 1});
+    Plot.limit(time);
+    Plot.label('Time (s)', 'Power (W)');
   end
 end
