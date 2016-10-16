eaed30bafdc56a68ab313b6341196ef91c500854
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-16 09:27:26 +0200

    Rewrite the compare script in Python

---
+++ bin/compare.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+
+from workload import Workload
+import constant
+import matplotlib.pyplot as pp
+import numpy as np
+
+def main(names, component_id=1):
+    count = len(names)
+    pp.figure(figsize=(14, 2 * count), facecolor='w', edgecolor='k')
+    for i in range(count):
+        data = Workload.aggregate(Workload.locate(names[i]), 'dynamic_power')
+        step_count = data.shape[1]
+        time = constant.TIME_STEP * np.arange(step_count)
+        pp.subplot(count, 1, i + 1)
+        pp.plot(time, data[component_id, :])
+        pp.xlim([time[0], time[-1]])
+        pp.xlabel('Time (s)')
+        pp.ylabel('Power (W)')
+    pp.show()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
